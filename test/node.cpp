17d67d910fdedd55469511835b5cdcf2ce393402
Adebola Adesina <dr3apap@gmail.com>
2022-06-28 13:47:18 -0700

    Re run tests on node abstraction for memory leak

--- test/node.cpp
+++ test/node.cpp
@@ -11,8 +11,8 @@ TEST(node, new_char_node)
     NodeType charNode = CHAR_NODE;
 
     EXPECT_EQ(node->type, charNode);
+    // memory free
     int result = Node_drop(node);
-    //EXPECT_TRUE(node == nullptr);
     EXPECT_EQ(result, 0);
 }
 
@@ -27,39 +27,46 @@ TEST(node, new_pair_node)
     NodeType char_Node = CHAR_NODE;
 
     EXPECT_EQ(node->type, pairNode); 
+
+    Node_drop(node);
+    EXPECT_FALSE(node->type == pairNode);
+
+}
+    
+
+TEST(node, new_error_node)
+{
+    Node *node = ErrorNode_new("This is an error");
+
+    NodeType errorNode = ERROR_NODE;
+
+    EXPECT_EQ(node->type, errorNode); 
+    Node_drop(node);
+    EXPECT_FALSE(node->type == errorNode);
+}
+
+TEST(node, node_pointer_free)
+{
+
+    Node *charNode =  CharNode_new('a');
+    Node *charNode1 =  CharNode_new('b');
+    Node *node =  PairNode_new(charNode, charNode1);
+
+
+    NodeType pairNode = PAIR_NODE;
+    NodeType char_Node = CHAR_NODE;
+
+    // Before memory free
+    EXPECT_TRUE(charNode->data.value == 'a');
+    EXPECT_TRUE(charNode1->data.value == 'b');
+
+    // Freeing the memory on the heap 
     Node_drop(node);
 
-
-    //EXPECT_TRUE(charNode->type == char_Node);
-    //EXPECT_TRUE(charNode1->type == char_Node);
-    //EXPECT_TRUE(node->type == pairNode);
-
-
-    EXPECT_TRUE(node == nullptr); 
-    EXPECT_TRUE(charNode == nullptr);
-    EXPECT_TRUE(charNode1 == nullptr);
+    // After memory free
+    EXPECT_FALSE(charNode->data.value == 'a');
+    EXPECT_FALSE(charNode1->data.value == 'b');
+    EXPECT_FALSE(node->type == pairNode);
+    EXPECT_FALSE(node->data.pair.left->type == char_Node); 
 }
-//    
-//TEST(node, new_error_node)
-//{
-//    Node *node = ErrorNode_new("This is an error");
-//
-//    NodeType errorNode = ERROR_NODE;
-//
-//    EXPECT_EQ(node->type, errorNode); 
-//    Node_drop(node);
-//    EXPECT_TRUE(node == nullptr);
-//}
-//
-//TEST(node, node_pointer_free)
-//{
-//
-//    Node *charNode = CharNode_new('a');
-//
-//    NodeType char_node = CHAR_NODE;
-//    
-//    Node_drop(charNode);
-//    EXPECT_TRUE(charNode == nullptr);
-//    //EXPECT_TRUE(charNode->type == jchar_node);
-//}
 
